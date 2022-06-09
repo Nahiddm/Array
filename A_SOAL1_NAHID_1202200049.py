486f74db2f581c3f2e60d63c967613e654d4f8e2
Nahiddm <107192854+Nahiddm@users.noreply.github.com>
2022-06-09 20:16:11 +0700

    Add files via upload

---
+++ A_SOAL1_NAHID_1202200049.py
@@ -0,0 +1,11 @@
+print("DATA DIRI MAHASISWA".center(50))
+datadiri={
+    "Nama": "AHMAD ",
+    "NIM":"12023332424",
+    "Kelas":"B1233",
+    "No.HP":"081XXXXX",
+}
+print("Nama\t:",datadiri["Nama"])
+print("NIM\t:",datadiri["NIM"])
+print("Kelas\t:",datadiri["Kelas"])
+print("No.HP\t:",datadiri["No.HP"])
